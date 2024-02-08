d86087a7608eccbd7db03ce3992ebdb11c61a8df
AzizjonIT <125983595+AzizjonIT@users.noreply.github.com>
2024-02-08 20:34:29 +0500

    #fix #added vercel analytics and vercel speedInsights

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
